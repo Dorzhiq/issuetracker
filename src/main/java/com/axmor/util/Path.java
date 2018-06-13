7fb03e9e1678cd236ca34f1e8ea5d25770b6e559
dorzhiq <dorzhiq@gmail.com>
2018-06-13 19:18:26 +0600

    Put complete 1

--- src/main/java/com/axmor/util/Path.java
+++ src/main/java/com/axmor/util/Path.java
@@ -3,8 +3,21 @@ package com.axmor.util;
 public class Path {
     public static class Web {
         public static final String ISSUES = "/issues/";
-        public static final String ISSUE_BY_ID = "/:issueId/";
-        public static final String PUT_ISSUE = "/issue/:issueId?author";
+        public static final String ISSUE_BY_ID = "/task/:issueId/";
+        public static final String PUT_ISSUE = "/issue/:issueId/";
+        public static final String POST_ISSUE = "/issues/add/";
+        public static final String CREATE_ISSUE = "/issues/create/";
+        public static final String DELETE_ISSUE = "/issues/delete/:issueId/";
+        public static final String LOGIN = "/login/";
+        public static final String LOGOUT = "/logout/";
+
+        public static String getLOGIN() {
+            return LOGIN;
+        }
+
+        public static String getLOGOUT() {
+            return LOGOUT;
+        }
 
         public static String getISSUES() {
             return ISSUES;
